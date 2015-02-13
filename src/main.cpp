fea2ef21f8e38671dfc174830ae2e154e884a310
kwketh <keth@kwketh.com>
2015-02-13 22:53:05 +0000

    initial commit with xcode project and explicit .gitignore

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  listdiff-cpp
+//
+//  Created by Konrad Wieczorek on 13/02/2015.
+//  Copyright (c) 2015 kwketh.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
